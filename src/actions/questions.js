ade14e6671b58dcc2f861d8f9927585375dcf249
Benjamin Currie <ben@benjamincurrie.com>
2021-04-21 11:57:39 -0400

    feat: Initial work on acitons, middleware and reducers
    Created initial skeleton work for the actions, middleware and reducers. Also added in an api file and data as well as loading in the initial data within App.js

---
+++ src/actions/questions.js
@@ -0,0 +1,57 @@
+import { saveQuestionAnswer, saveQuestion } from '../utils/api'
+import { showLoading, hideLoading } from 'react-redux-loading'
+
+export const RECEIVE_QUESTIONS = 'RECEIVE_QUESTIONS'
+export const ANSWER_QUESTION = 'ANSWER_QUESTION'
+export const ADD_QUESTION = 'ADD_QUESTION'
+
+function addQuestion(question) {
+  return {
+    type: ADD_QUESTION,
+    question,
+  }
+}
+
+export function handleAddQuestion(text) {
+  return (dispatch, getState) => {
+    const { authedUser } = getState()
+    dispatch(showLoading())
+
+    return saveQuestion({
+      text,
+      author: authedUser
+    })
+      .then((question) => dispatch(addQuestion(question)))
+      .then(() => dispatch(hideLoading()))
+  }
+}
+
+export function receiveQuestions(questions) {
+  return {
+    type: RECEIVE_QUESTIONS,
+    questions,
+  }
+}
+
+export function answerQuestion({ id, authedUser, answer }) {
+  return {
+    type: ANSWER_QUESTION,
+    id,
+    authedUser,
+    answer
+  }
+}
+
+export function handleAnswerQuestion(info) {
+  return (dispatch) => {
+    dispatch(answerQuestion(info))
+
+    return saveQuestionAnswer(info)
+      .catch((e) => {
+        console.warn('Error in handleAnswerQuestion: ', e)
+        // Todo need to set answer back to previous
+        // dispatch(answerQuestion(info))
+        alert('There was an error saving your answer, try again.')
+      })
+  }
+}
---
+++ src/screens/Login.js
@@ -0,0 +1,101 @@
+
+import React, { Component } from 'react'
+import { connect } from 'react-redux'
+
+class Login extends Component {
+  render() {
+    return (
+      <div className="min-h-screen flex items-center justify-center bg-gray-50 py-12 px-4 sm:px-6 lg:px-8" >
+        <div className="max-w-md w-full space-y-8">
+          <div>
+            <img className="mx-auto h-12 w-auto" src="https://tailwindui.com/img/logos/workflow-mark-indigo-600.svg" alt="Workflow" />
+            <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+              Sign in to your account
+            </h2>
+            <p className="mt-2 text-center text-sm text-gray-600">
+              Or
+              <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                start your 14-day free trial
+              </a>
+            </p>
+          </div>
+          <form className="mt-8 space-y-6" action="#" method="POST">
+            <input type="hidden" name="remember" value="true" />
+
+            <div>
+              <label id="listbox-label" className="block text-sm font-medium text-gray-700">
+                Assigned to
+            </label>
+              <div className="mt-1 relative">
+                <button type="button" className="relative w-full bg-white border border-gray-300 rounded-md shadow-sm pl-3 pr-10 py-2 text-left cursor-default focus:outline-none focus:ring-1 focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm" aria-haspopup="listbox" aria-expanded="true" aria-labelledby="listbox-label">
+                  <span className="flex items-center">
+                    <img src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" className="flex-shrink-0 h-6 w-6 rounded-full" />
+                    <span className="ml-3 block truncate">
+                      Tom Cook
+                    </span>
+                  </span>
+                  <span className="ml-3 absolute inset-y-0 right-0 flex items-center pr-2 pointer-events-none">
+                    <svg className="h-5 w-5 text-gray-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                      <path fillRule="evenodd" d="M10 3a1 1 0 01.707.293l3 3a1 1 0 01-1.414 1.414L10 5.414 7.707 7.707a1 1 0 01-1.414-1.414l3-3A1 1 0 0110 3zm-3.707 9.293a1 1 0 011.414 0L10 14.586l2.293-2.293a1 1 0 011.414 1.414l-3 3a1 1 0 01-1.414 0l-3-3a1 1 0 010-1.414z" clipRule="evenodd" />
+                    </svg>
+                  </span>
+                </button>
+
+                <ul className="absolute mt-1 w-full bg-white shadow-lg max-h-56 rounded-md py-1 text-base ring-1 ring-black ring-opacity-5 overflow-auto focus:outline-none sm:text-sm" tabIndex="-1" role="listbox" aria-labelledby="listbox-label" aria-activedescendant="listbox-option-3">
+                  <li className="text-gray-900 cursor-default select-none relative py-2 pl-3 pr-9" id="listbox-option-0" role="option">
+                    <div className="flex items-center">
+                      <img src="https://images.unsplash.com/photo-1491528323818-fdd1faba62cc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt="" className="flex-shrink-0 h-6 w-6 rounded-full" />
+                      <span className="font-normal ml-3 block truncate">
+                        Wade Cooper
+                    </span>
+                    </div>
+
+                    <span className="text-indigo-600 absolute inset-y-0 right-0 flex items-center pr-4">
+                      <svg className="h-5 w-5" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                        <path fillRule="evenodd" d="M16.707 5.293a1 1 0 010 1.414l-8 8a1 1 0 01-1.414 0l-4-4a1 1 0 011.414-1.414L8 12.586l7.293-7.293a1 1 0 011.414 0z" clipRule="evenodd" />
+                      </svg>
+                    </span>
+                  </li>
+
+                </ul>
+              </div>
+            </div>
+
+            <div className="flex items-center justify-between">
+              <div className="flex items-center">
+                <input id="remember_me" name="remember_me" type="checkbox" className="h-4 w-4 text-indigo-600 focus:ring-indigo-500 border-gray-300 rounded" />
+                <label htmlFor="remember_me" className="ml-2 block text-sm text-gray-900">
+                  Remember me
+              </label>
+              </div>
+
+              <div className="text-sm">
+                <a href="#" className="font-medium text-indigo-600 hover:text-indigo-500">
+                  Forgot your password?
+              </a>
+              </div>
+            </div>
+
+            <div>
+              <button type="submit" className="group relative w-full flex justify-center py-2 px-4 border border-transparent text-sm font-medium rounded-md text-white bg-indigo-600 hover:bg-indigo-700 focus:outline-none focus:ring-2 focus:ring-offset-2 focus:ring-indigo-500">
+                <span className="absolute left-0 inset-y-0 flex items-center pl-3">
+                  <svg className="h-5 w-5 text-indigo-500 group-hover:text-indigo-400" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" aria-hidden="true">
+                    <path fillRule="evenodd" d="M5 9V7a5 5 0 0110 0v2a2 2 0 012 2v5a2 2 0 01-2 2H5a2 2 0 01-2-2v-5a2 2 0 012-2zm8-2v2H7V7a3 3 0 016 0z" clipRule="evenodd" />
+                  </svg>
+                </span>
+                Sign in
+            </button>
+            </div>
+          </form>
+        </div>
+      </div>
+    )
+  }
+}
+
+function mapStateToProps() {
+  return {
+  }
+}
+
+export default connect()(Login)
